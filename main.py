43955c1507fd9acdd34ca86e40dc97af31bb72ea
Dhruv <dhruvdass2005@gmail.com>
2025-09-16 23:48:15 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# This is my First python comment
+print("I Like Burgir!")
+print("It's really Tasty")
---
+++ variables.py
@@ -0,0 +1,51 @@
+# variables -> Containers to store values.
+
+# String
+first_name = "Dhruv"
+food = "Burgir"
+email = "dd@gmail.com"
+# print(first_name)
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Contact: {email}")
+print("")
+
+# Integers
+age = 19
+quantity = 6
+students = 20
+print(f"You are {age} years old")
+print(f"You are buying {quantity} Items")
+print(f"Your class now has {students} students")
+print("")
+
+# Float
+price = 10.99
+gpa = 3.9
+distance = 6.9
+
+print(f"You paid ${price}")
+print(f"Your GPA is: {gpa}")
+print(f"Your college is {distance} kms away")
+print("")
+
+# Booleans
+
+is_student = True
+for_sale = False
+is_online = True
+
+# if is_student:
+#     print("You are a student")
+# else:
+#     print("You are not a student")
+
+# if for_sale:
+#     print("Available for sale")
+# else:
+#     print("That is NOT available for sale")
+
+if is_online:
+    print("Online")
+else:
+    print("Offline")
